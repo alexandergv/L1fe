0597aa67a1181e7efba63d735aebe02fe040473a
alexander <alexandergv04@gmail.com>
2022-09-15 12:45:09 -0400

    handleChange on Inputs

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,25 +1,42 @@
-import { Component } from "react";
+import { useState } from "react";
 
-class Login extends Component {
-  render() {
-    return (
-      <div className="login-page">
-        <div className="login-text"></div>
-        <form className="login-form">
-          <input
-            className="login-username"
-            type="text"
-            placeholder="username"
-          />
-          <input
-            type="text"
-            placeholder="password"
-            className="login-password"
-          />
-          <input className="login-submit" type="submit" value="Login" />
-        </form>
-      </div>
-    );
-  }
-}
+const Login = () => {
+  const user = {
+    username: "",
+    password: "",
+  };
+
+  const [state, setState] = useState();
+
+  const handleChange = (element, type) => {
+    if (type === "username") {
+      user.username = element.target.value;
+    } else {
+      user.password = element.target.value;
+    }
+    setState("NuevoVideo");
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-text"></div>
+      <form className="login-form">
+        <input
+          className="login-username"
+          type="text"
+          placeholder="username"
+          onChange={(e) => handleChange(e, "username")}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="login-password"
+          onChange={(e) => handleChange(e, "password")}
+        />
+        {state}
+        <input className="login-submit" type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
 export default Login;
